1fb4bbe368c96b82fdff00f6ea5ac8438a21b909
Munsur Hallaj Sohan <munsursohan0906a@gmail.com>
2026-04-20 12:44:58 +0600

    Add files via upload

---
+++ api_calling.py
@@ -0,0 +1,43 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+import io
+from gtts import gTTS
+#loading environment variable
+load_dotenv()
+my_api_key=os.getenv("Gemini_Api_Key")
+
+#initialize client
+
+client=genai.Client(api_key=my_api_key)
+
+#note genartor function
+
+def note_generator(images):
+    prompt="""summarize the picture in note formate at max 100 words,
+              make sure markdown to differntiate diffarent section"""
+    response=client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=[images,prompt]
+    )
+
+    return response.text
+
+#audio
+
+def audio_transcript(text):
+    speach=gTTS(text,lang='en',slow=False)
+    audio_buffer=io.BytesIO()
+    speach.write_to_fp(audio_buffer)
+    return audio_buffer
+
+#qiuz
+
+def quize_generator(images,difficulty):
+    prompt=f"Create 3 quizes and based on the {difficulty}.make sure markdown to differntiate diffarent section"
+    response=client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=[images,prompt]
+    )
+
+    return response.text
---
+++ app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from api_calling import note_generator,audio_transcript,quize_generator
+from PIL import Image
+
+
+#title
+st.title("Note Summary And Quize Generator")
+st.markdown("Upload 3 image to genarate note summary and genearte Quize")
+st.divider()
+
+#sidebar
+with st.sidebar:
+    #image
+    images=st.file_uploader("Upload yours Note Photo",
+                            type=['jpg','png','jpeg'],
+                            accept_multiple_files=True)
+    
+    #convert pil images
+    pil_images=[]
+    for img in images:
+        pil_img=Image.open(img)
+        pil_images.append(pil_img)
+
+    if images:
+        if len(images)>3:
+            st.error("Upload only 3 images")
+        else:
+            # st.image(image)
+            col=st.columns(len(images))
+
+            for i,img in enumerate(images):
+                with col[i]:
+                    st.image(img) 
+
+    #catagory
+    option=st.selectbox(
+        "Enter the Difficulty of your quize",
+         ("Easy","Medium","Hard"),
+         index=None
+        )
+    # if option:
+    #     st.markdown(f"You selected: **{option}**")
+    # else:
+    #     st.error("You must select Option")
+    
+    button=st.button("Click the button to intiate AI")
+
+
+if button:
+    if not images:
+        st.error("Upolad images")
+    if not option:
+        st.error("Select Diffucalty")
+    if images and option:
+        #note
+
+        with st.container(border=True):
+            st.subheader("Your Note")
+            with st.spinner("AI is writing note"):
+                 note=note_generator(pil_images)
+                 st.markdown(note)
+        #audio
+        with st.container(border=True):
+            st.subheader("Audio Transcript")
+            with st.spinner("Auido making"):
+                note=note.replace("#","")
+                note=note.replace("$","")
+                note=note.replace("*","")
+                note=note.replace("`","")
+                note=note.replace("-","")
+                st.audio(audio_transcript(note))
+        #quize
+        with st.container(border=True): 
+            st.subheader(f"Quize ({option}) Diffculty")
+            with st.spinner("AI is writing note"):
+                 st.markdown(quize_generator(pil_images,option))
